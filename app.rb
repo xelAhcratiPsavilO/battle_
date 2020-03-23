c6e571d27ef6af4dc6dff9d4932cfcf7381b5ea2
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-23 22:38:57 +0000

    Create play view and add necessary routes

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
