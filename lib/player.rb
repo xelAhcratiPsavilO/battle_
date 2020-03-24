4ae9a74e319829a0cfda03903c20d0e3c41abb19
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-24 08:57:16 +0000

    Initialize player with default hit points

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,11 @@
 
 class Player
-  attr_reader :name
+  DEFAULT_HIT_POINTS = 60
+  attr_reader :name, :hit_points
 
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
       @name = name
+      @hit_points = hit_points
   end
 
 end
