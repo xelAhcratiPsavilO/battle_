6d9cffb2a5c18e092161d54dfdf5bbd7d9eb7c55
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-24 08:53:47 +0000

    Test default hit points of the class player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,17 @@ require 'player'
 
 describe Player do
   subject(:alex) { Player.new('Alex') }
+  subject(:christina) { Player.new('Christina') }
 
   describe '#name' do
     it 'returns its name' do
       expect(alex.name).to eq 'Alex'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(alex.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
 end
