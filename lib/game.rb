a2622fd814e444a56851ece2bd4b939b6453d2a4
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-24 19:48:20 +0000

    Refactor controller

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,19 @@
 
 class Game
 
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
   def attack(player)
     player.receive_damage
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
   end
 end
