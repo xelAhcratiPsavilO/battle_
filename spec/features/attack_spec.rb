e142563e037b5127a2c8c5fe15cc1b2b9d2e0dcd
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-24 00:01:18 +0000

    Test decrease in hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,16 @@
 feature 'Attacking' do
+
   scenario 'attack Player 2' do
     sign_in_and_play
     click_link 'Attack'
     expect(page).to have_content 'Me attacked You'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'You: 60HP'
+    expect(page).to have_content 'You: 50HP'
   end
 end
