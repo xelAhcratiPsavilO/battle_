67142a553af6a6adb227561c5acbad39014eed67
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-29 13:29:38 +0100

    Test Game Over

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,10 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new(player_1, player_2) }
-  let(:player_1) { double :player }
-  let(:player_2) { double :player }
+  subject(:finished_game) { described_class.new(dead_player, player_2) }
+  let(:player_1) { double :player, hit_points: 60 }
+  let(:player_2) { double :player, hit_points: 60 }
+  let(:dead_player) { double :player, hit_points: 0 }
 
   describe '#player_1' do
     it 'retrieves the first player' do
@@ -30,16 +32,32 @@ describe Game do
     end
   end
 
-  # describe '#awaiting_turn' do
-  #   it 'starts as player 2' do
-  #     expect(game.awaiting_turn).to eq player_2
-  #   end
-  # end
+  describe '#awaiting_turn' do
+    it 'starts as player 2' do
+      expect(game.awaiting_turn).to eq player_2
+    end
+  end
 
   describe '#switch_turns' do
     it 'switches the turn' do
       game.switch_turns
       expect(game.current_turn).to eq player_2
+    end
+  end
+
+  describe '#game_over?' do
+    it 'returns false if no-one is at 0HP' do
+      expect(game.game_over?).to be false
+    end
+
+    it 'returns true if at least one player is at 0HP' do
+      expect(finished_game.game_over?).to be true
+    end
+  end
+
+  describe '#loser' do
+    it 'returns a player that reached 0HP' do
+      expect(finished_game.loser).to eq dead_player
     end
   end
 
