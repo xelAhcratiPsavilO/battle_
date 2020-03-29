bacc0bbb66f40cdcdd54b2329243aff3bed6ca9b
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-29 11:32:36 +0100

    Implement Player2 awaits turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 
 class Game
-  attr_reader :current_turn
+  attr_reader :current_turn, :awaiting_turn
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_turn = player_1
+    @awaiting_turn = opponent_of(current_turn)
   end
 
   def attack(player)
