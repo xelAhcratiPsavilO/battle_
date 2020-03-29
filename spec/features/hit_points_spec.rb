f50ae44aaa13cfcad70c2f8790749e38b48389c5
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-03-29 11:09:43 +0100

    Test Player1 hit points display

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,8 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Alex: 60HP'
   end
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Christina: 60HP'
+  end
 end
